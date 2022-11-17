edfd168b172508a76c40f89396d57e908287bf4c
Wh0xer <natanaelvieira1@outlook.com>
2022-11-16 22:13:54 -0300

    Alterei o nome da do diretório para

---
+++ Jogo de Adivinhação/target/debug/deps/guessing_game-a6f2637aae0bee17.d
@@ -0,0 +1,5 @@
+/home/natanael/learningRust/guessing_game/target/debug/deps/guessing_game-a6f2637aae0bee17: src/main.rs
+
+/home/natanael/learningRust/guessing_game/target/debug/deps/guessing_game-a6f2637aae0bee17.d: src/main.rs
+
+src/main.rs:
---
+++ Jogo de Adivinhação/target/debug/deps/rand_chacha-85b6dcfaa15d7511.d
@@ -0,0 +1,9 @@
+/home/natanael/learningRust/guessing_game/target/debug/deps/rand_chacha-85b6dcfaa15d7511.rmeta: /home/natanael/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/natanael/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/natanael/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
+
+/home/natanael/learningRust/guessing_game/target/debug/deps/librand_chacha-85b6dcfaa15d7511.rlib: /home/natanael/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/natanael/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/natanael/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
+
+/home/natanael/learningRust/guessing_game/target/debug/deps/rand_chacha-85b6dcfaa15d7511.d: /home/natanael/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/natanael/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/natanael/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
+
+/home/natanael/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs:
+/home/natanael/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs:
+/home/natanael/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs:
