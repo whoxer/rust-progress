f3c6e9d9aa53f3c66ca2800380988569b30f5efa
Wh0xer <natanaelvieira1@outlook.com>
2022-11-16 22:14:21 -0300

    Revert "Alterei o nome da do diretório para"
    This reverts commit edfd168b172508a76c40f89396d57e908287bf4c.

--- Jogo de Adivinhação/target/debug/build/libc-dc9de90290b6f71c/build_script_build-dc9de90290b6f71c.d
+++
@@ -1,5 +0,0 @@
-/home/natanael/learningRust/guessing_game/target/debug/build/libc-dc9de90290b6f71c/build_script_build-dc9de90290b6f71c: /home/natanael/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.137/build.rs
-
-/home/natanael/learningRust/guessing_game/target/debug/build/libc-dc9de90290b6f71c/build_script_build-dc9de90290b6f71c.d: /home/natanael/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.137/build.rs
-
-/home/natanael/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.137/build.rs:
--- Jogo de Adivinhação/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/natanael/learningRust/guessing_game/target/debug/guessing_game: /home/natanael/learningRust/guessing_game/src/main.rs
